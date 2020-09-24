54a5aa1cfbe12afbcc910fce7fb803319175370e
junior <eecfredes@gmail.com>
2020-09-24 12:37:09 -0300

    002

---
+++ exercicios/001/app.js
@@ -0,0 +1,7 @@
+const nome = "junior 2000";
+var f = (3 % 2) + 4 / 2;
+console.log(`meu nome é ${nome}`);
+console.log(`O valor é ${f}`);
+
+var username = "craig";
+console.log(`hello ${username}`);
