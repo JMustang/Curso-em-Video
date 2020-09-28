ebc6167f9f2bba8f6c11a40c0386d1423e623300
junior <eecfredes@gmail.com>
2020-09-28 18:59:23 -0300

    005

---
+++ exercicios/005/app.js
@@ -0,0 +1,36 @@
+contar = () => {
+  let inicio = document.getElementById("txti");
+  let fim = document.getElementById("txtf");
+  let passo = document.getElementById("txtp");
+  let res = document.getElementById("res");
+
+  //Controle de erros
+  if (
+    inicio.value.length == 0 ||
+    fim.value.length == 0 ||
+    passo.value.length == 0
+  ) {
+    res.innerHTML = "Impossivel contar!";
+    //window.alert("[ERRO] Faltam dados!");
+  } else {
+    res.innerHTML = "contando... <br> ";
+    let i = Number(inicio.value);
+    let f = Number(fim.value);
+    let p = Number(passo.value);
+    if (p <= 0) {
+      window.alert("Passo invalido! Considerando PASSO 1");
+      p = 1;
+    }
+    if (i < f) {
+      //contagem crescente
+      for (let c = i; c <= f; c += p) {
+        res.innerHTML += ` ${c}, `;
+      }
+    } else {
+      //contagem regressiva
+      for (let c = i; c >= f; c -= p) {
+        res.innerHTML += ` ${c}, `;
+      }
+    }
+  }
+};
